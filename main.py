1d38d5218b8742f9fd8e43c6f65a3703e16feb01
Abhishek kushwaha <abhishekkushwaha1479@gmail.com>
2022-02-02 00:56:45 +0530

    added some comments for easy understanding 😁

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ from pydantic import BaseModel
 
 app = FastAPI()
 
+#Schema 
 class Post(BaseModel):
     # post_id:int = random[0,1000000000] #will generate a random number as post_id (only for testing)
     content: str #the main content of the post 
@@ -16,11 +17,13 @@ class Post(BaseModel):
     reacts: int = 0 # its the likes that the post have 
     created_at: str = datetime.now() # its the time at which the post was created 
 
+
+# root operation 
 @app.get("/")
 def read_root():
     return {"Title":"SOCIOME- Social Media Api","Creator":"Code For Community", "Founded_By":"Abhishek Kushwaha" ,"Version":"1.0.0","Started_AT":"Janurary 2022"}
 
-
+# This path shows all the developers of the *Sociome* with their github accounts 
 @app.get("/developers")
 def read_item():
     return {1:{"Name": "Abhishek Kushwaha", "Github_id": "https://github.com/Abbhiishek"},
@@ -34,6 +37,7 @@ def read_item():
     9:{"Name": "Koushik Das", "Github_id": "https://github.com/koushik-das123"},
     }
 
+# This path operation is to send a custom messgae to the api and retrieve it (just for learning purpose !)
 @app.get("/CustomeMsg/{author_id}/{msg}/{Author_Name}")
 def read_item(author_id: int, msg: Optional[str] = None, Author_Name:Optional[str]= None):
     return {"Author_Id": author_id,"Author_Name": Author_Name, "Message": msg }
@@ -44,18 +48,23 @@ def read_item(author_id: int, msg: Optional[str] = None, Author_Name:Optional[st
     "Message": "hello i am testing the api"
     }'''
 
+# This operation delivers the current iso formatted time of the post requests !
 @app.get("/time")
 def read_item():
     time = datetime.today()
     isoTime= time.isoformat()
     return {"Today Date": isoTime}
 
+# This path OPERATION is to retrieve all the post from the data base @
 @app.get("/posts") #it get all the posts
 def get_posts():
     return{"Posts":{1:{"Name":"SOCIOME Posts 01"},
                     2:{"Name":"Amandeep"},
                     }}
-@app.post("/create_posts") #it create a single post 
+
+
+#This path operation create a single post 
+@app.post("/create_posts") 
 def create_posts(content:Post): # we are passing the information that we need to pass from body as dictionary and store it in content 
     return{"Status":"Posts Created !" , "Posts":content} # reteriving the body content , but its not being stored now !!!
 
