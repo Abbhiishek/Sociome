1bc1db2b2b27e177a7170432798b5131c242ff80
Abhishek kushwaha <86338762+Abbhiishek@users.noreply.github.com>
2022-02-01 20:27:05 +0530

    Updated the Root , Developers , Custom Message
    developers are added ./
    custom message request is added ./
    About the Api is done in root path ./

--- main.py
+++ main.py
@@ -9,20 +9,28 @@ app = FastAPI()
 
 @app.get("/")
 def read_root():
-    return {"Messgaes": "Hey viewer ! i made it man !","Author":"Abhishek Kushwaha"}
+    return {"Title":"SOCIOME- Social Media Api","Creator":"Code For Community", "Founded_By":"Abhishek Kushwaha" ,"Version":"1.0.0","Started_AT":"Janurary 2022"}
 
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Optional[str] = None):
-    return {"item_id": item_id, "q": q}
+@app.get("/developers")
+def read_item():
+    return {1:{"Name": "Abhishek Kushwaha", "Github_id": "https://github.com/Abbhiishek"},
+    2:{"Name": "Amandeep Singh", "Github_id": "https://github.com/Aman8017k"},
+    3:{"Name": "Shivam Basak", "Github_id": "https://github.com/shivamBasak"},
+    4:{"Name": "Anjali Gupta", "Github_id": "https://github.com/anjalig18"},
+    5:{"Name": "Anshita Choubey", "Github_id": "https://github.com/Anshitachoubey07"},
+    6:{"Name": "Muskaan Purkait Alam", "Github_id": "https://github.com/Muskaanpurkait"},
+    7:{"Name": "Priya Singh", "Github_id": "https://github.com/Priyasinghjis"},
+    8:{"Name": "Roshimkana Pal", "Github_id": "https://github.com/Abbhiishek"},
+    9:{"Name": "Koushik Das", "Github_id": "https://github.com/koushik-das123"},
+    }
 
-@app.get("/author/{author_id}/{q}")
-def read_item(author_id: int, q: Optional[str] = None):
-    return {"item_id": author_id, "request": q ,"name":"Abhishek kushwaha"}
+@app.get("/CustomeMsg/{author_id}/{msg}/{Author_name}")
+def read_item(author_id: int, q: Optional[str] = None, name:Optional[str]= None):
+    return {"Author_Id": author_id,"Author_Name": Author_name, "Message": msg }
 
 @app.get("/time")
 def read_item():
     time = datetime.today()
     isoTime= time.isoformat()
     return {"Today Date": isoTime}
-
