6c826b5b9a74131d3ef35d33fb5b13c89d2cf82b
Abhishek kushwaha <abhishekkushwaha1479@gmail.com>
2022-02-06 20:02:33 +0530

    added more functionality

--- app/main.py
+++ app/main.py
@@ -1,7 +1,9 @@
 import datetime
 from distutils.log import error
+from http.client import HTTPException
 from random import randrange
 from sqlite3 import DatabaseError
+from urllib import response
 from fastapi.params import Body
 from typing import Optional
 import time
@@ -11,7 +13,7 @@ from pydantic import BaseModel
 import psycopg2
 from psycopg2.extras import RealDictCursor
 import time
-from . import developers , CustomMsg
+from . import developers , CustomMsg , Schemas
  
 app = FastAPI()
 while True:
@@ -27,19 +29,6 @@ while True:
         print ("Connecting to database failed")
         print("Error: ", error)
         time.sleep(2)
-
-
-#Schema 
-class Post(BaseModel):
-    post_id: Optional[int]   #will generate a random number as post_id (only for testing)
-    content: str #the main content of the post 
-    published: bool =True #the post will be published or not default is set to true 
-    reacts: int = 0 # its the likes that the post have 
-    created_at: str = datetime.now() # its the time at which the post was created 
-    image: Optional[str]
-#fake post dictionary
-
-post=[{}]
 
 # root operation 
 @app.get("/")
@@ -58,19 +47,46 @@ def read_item():
 def get_posts():
     cur.execute("""SELECT * FROM posts """)
     posts=cur.fetchall()
-    print(posts)
     return{"Posts":posts}
 
 
 #This path operation create a single post 
 @app.post("/create_posts", status_code=status.HTTP_201_CREATED) 
-def create_posts(content:Post):
+def create_posts(content:Schemas.PostBase):
     # we are passing the information that we need to pass from body as dictionary and store it in content 
     # cur.exectue(f"INSERT INTO posts (title, content, published) VALUES({post.title}, {post.content})")
     cur.execute("""INSERT INTO posts (content, published) VALUES (%s, %s) RETURNING * """,(content.content, content.published))
     created_post = cur.fetchone()
     conn.commit()
     return {"Created post ": created_post}
+
+
+#This path operation is to find a single post already in the database by the post id :
+@app.get("/posts/{id}", status_code=status.HTTP_302_FOUND)
+def get_post(id : int):
+    print(id)
+    id=str(id)
+    cur.execute("""SELECT * FROM posts WHERE post_id =(%s)""",(id))
+    post=cur.fetchone()
+    if not post:
+        response.status_code = status.HTTP_404_NOT_FOUND
+        return {"Status": "Post with an id of "+ id +" is not Found ."}
+    return {"Status": "The post Id that you have choosen to retrieve is "+id,"ASKED Post":post}
+
+# This path operation is to delete a single post by its id 
+
+@app.delete("/posts/{id}", status_code=status.HTTP_204_NO_CONTENT)
+def delete_post(id : int):
+    print(id)
+    id=str(id)
+    cur.execute("""SELECT * FROM posts WHERE post_id =(%s)""",(id))
+    post=cur.fetchone()
+    if not post:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                       details=f"post with id: {id} was not found")
+    cur.execute("""DELETE FROM posts WHERE post_id = (%s) """,(id))
+    conn.commit()
+    return {"Status": "The post Id that you have choosen to delete is "+id,"Deleted Post":post}
 
 
 
