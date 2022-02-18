c148707aecfbac6d634aa432825479179c9cd00b
Abhishek kushwaha <abhishekkushwaha1479@gmail.com>
2022-02-18 23:23:02 +0530

    commented out the raw sql

--- app/Routers/post.py
+++ app/Routers/post.py
@@ -15,26 +15,28 @@ router= APIRouter(prefix="/posts",
     tags=['Posts'])
 
 
-while True:
-    try:
-        # Connect to an existing database
-        conn =psycopg2.connect(host='localhost', database='Sociome', user='postgres',password='abhishek1234',cursor_factory=RealDictCursor)
+#commented out  as we are going to use the sqlalchemy instance to perform database operation
 
-        # Open a cursor to perform database operations
-        cur = conn.cursor()
-        print("Connection With Database is established !")
-        break
-    except Exception as error:
-        print ("Connecting to database failed")
-        print("Error: ", error)
-        time.sleep(2)
+# while True:
+#     try:
+#         # Connect to an existing database
+#         conn =psycopg2.connect(host='localhost', database='Sociome', user='postgres',password='abhishek1234',cursor_factory=RealDictCursor)
+
+#         # Open a cursor to perform database operations
+#         cur = conn.cursor()
+#         print("Connection With Database is established !")
+#         break
+#     except Exception as error:
+#         print ("Connecting to database failed")
+#         print("Error: ", error)
+#         time.sleep(2)
 
 # This path OPERATION is to retrieve all the post from the data base @
 @router.get("/") #it get all the posts
 def get_posts():
-    cur.execute("""SELECT * FROM posts  ORDER BY created_at DESC""")
-    posts=cur.fetchall()
-    return{"Posts":posts}
+    # cur.execute("""SELECT * FROM posts  ORDER BY created_at DESC""")
+    # posts=cur.fetchall()
+    return posts
 
 
 #This path operation create a single post 
@@ -42,17 +44,23 @@ def get_posts():
 def create_posts(content:Schemas.PostBase):
     # we are passing the information that we need to pass from body as dictionary and store it in content 
     # cur.exectue(f"INSERT INTO posts (title, content, published) VALUES({post.title}, {post.content})")
-    cur.execute("""INSERT INTO posts (content, published ,image) VALUES (%s, %s , %s) RETURNING * """,(content.content, content.published,content.image))
-    created_post = cur.fetchone()
-    conn.commit()
+    # cur.execute("""INSERT INTO posts (content, published ,image) VALUES (%s, %s , %s) RETURNING * """,(content.content, content.published,content.image))
+    # created_post = cur.fetchone()
+    # conn.commit()
+
+
+
     return {"Created post ": created_post}
 
 
 #This path operation is to find a single post already in the database by the post id :
 @router.get("/{id}", status_code=status.HTTP_302_FOUND)
 def get_post(id : int):
-    cur.execute(""" SELECT * FROM posts WHERE post_id = %s """, (str(id),))
-    post=cur.fetchone()
+    # cur.execute(""" SELECT * FROM posts WHERE post_id = %s """, (str(id),))
+    # post=cur.fetchone()
+
+
+
     if not post:
         response.status_code = status.HTTP_404_NOT_FOUND
         return {"Status": "Post with an id of "+ id +" is not Found ."}
@@ -62,23 +70,30 @@ def get_post(id : int):
 
 @router.delete("/{id}", status_code=status.HTTP_204_NO_CONTENT)
 def delete_post(id : int):
-    cur.execute("""SELECT * FROM posts WHERE post_id = %s RETURNING * """, (str(id),))
-    post=cur.fetchone()
+    # cur.execute("""SELECT * FROM posts WHERE post_id = %s RETURNING * """, (str(id),))
+    # post=cur.fetchone()
+
+
     if not post:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
                        details=f"post with id: {id} was not found")
-    cur.execute("""DELETE FROM posts WHERE post_id = (%s) """,(id))
-    conn.commit()
+    # cur.execute("""DELETE FROM posts WHERE post_id = (%s) """,(id))
+    # conn.commit()
+
+
     return {"Status": "The post Id that you have choosen to delete is "+str(id),"Deleted Post":post}
 
 
 
 @router.put("/{id}" , status_code=status.HTTP_201_CREATED)
 def update_post(id : int, content:Schemas.PostBase):
-    cur.execute(""" UPDATE posts SET  content = %s , published = %s ,image =%s WHERE post_id = %s RETURNING * """,(content.content, content.published,content.image,str(id),))
-    post=cur.fetchone()
+    # cur.execute(""" UPDATE posts SET  content = %s , published = %s ,image =%s WHERE post_id = %s RETURNING * """,(content.content, content.published,content.image,str(id),))
+    # post=cur.fetchone()
     if not post:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
                        details=f"post with id: {id} was not found")
-    conn.commit()
+    # conn.commit()
+
+
+    
     return {"Status": "The post Id with  {id} was deletd succesfully ","Deleted Post":post}
