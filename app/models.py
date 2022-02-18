865b36696a17f32236187681272a82c6c38af832
Abhishek kushwaha <abhishekkushwaha1479@gmail.com>
2022-02-19 00:13:32 +0530

    fixed the cloumn names as per database

--- app/models.py
+++ app/models.py
@@ -1,3 +1,4 @@
+from email.mime import image
 from sqlalchemy import Column, Integer, String, Boolean
 from sqlalchemy.sql.expression import text
 from sqlalchemy.sql.sqltypes import TIMESTAMP
@@ -7,8 +8,9 @@ class Post(Base):
 
     __tablename__= "posts"
 
-    id = Column (Integer, primary_key=True, nullable=False)
-    title = Column(String, nullable=False)
+    post_id = Column (Integer, primary_key=True, nullable=False)
     content = Column(String, nullable=False)
+    reacts = Column(Integer, nullable=False, server_default='0')
+    image = Column(String)
     published = Column (Boolean, server_default='TRUE', nullable=False)
     created_at = Column(TIMESTAMP(timezone=True), nullable=False, server_default=text('now()'))
