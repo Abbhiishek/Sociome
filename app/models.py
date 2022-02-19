9c31763d98c8e3cc0cbebe79039c6713e4c6d5ec
Abhishek kushwaha <abhishekkushwaha1479@gmail.com>
2022-02-19 18:35:56 +0530

    new user database model added

--- app/models.py
+++ app/models.py
@@ -1,9 +1,11 @@
+from ast import Str
 from email.mime import image
 from sqlalchemy import Column, Integer, String, Boolean
 from sqlalchemy.sql.expression import text
 from sqlalchemy.sql.sqltypes import TIMESTAMP
 from .database import Base
 
+# This is the structure of Post table in the database 
 class Post(Base):
 
     __tablename__= "posts"
@@ -14,3 +16,18 @@ class Post(Base):
     image = Column(String)
     published = Column (Boolean, server_default='TRUE', nullable=False)
     created_at = Column(TIMESTAMP(timezone=True), nullable=False, server_default=text('now()'))
+
+# This is the structure of user table in the database 
+class User(Base):
+
+    __tablename__= "user"
+    user_id = Column (Integer, primary_key=True, nullable=False)
+    email = Column (String , nullable=False, unique=True)
+    password = Column(String, nullable=False)
+    username = Column(String, nullable=False, unique=True)
+    fullname = Column(String)
+    profile_pic = Column(String)
+    bio = Column(String)
+    github_link = Column(String)
+    joined_at = Column(TIMESTAMP(timezone=True), nullable=False, server_default=text('now()'))
+
