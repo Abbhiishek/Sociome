04436795d8c5955146649875e1ef92aaa246ff13
Abhishek kushwaha <abhishekkushwaha1479@gmail.com>
2022-02-19 18:44:01 +0530

    password hashing ....

---
+++ app/Routers/hashing.py
@@ -0,0 +1,12 @@
+
+from passlib.context import CryptContext
+
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+
+def hash(password):
+    return pwd_context.hash(password)
+
+
+def verify_password(plain_password, hashed_password):
+    return pwd_context.verify(plain_password, hashed_password)
