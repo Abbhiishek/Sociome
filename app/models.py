f9acd14e41b53b3d5135dc60372b60ae255c3f36
Abhishek kushwaha <abhishekkushwaha1479@gmail.com>
2022-02-19 19:41:40 +0530

    added the foreign key to posts and connected to users via author

--- app/models.py
+++ app/models.py
@@ -1,8 +1,6 @@
-from ast import Str
-from email.mime import image
-from sqlalchemy import Column, Integer, String, Boolean
+from sqlalchemy import Column,Integer, String, Boolean,ForeignKey
 from sqlalchemy.sql.expression import text
-from sqlalchemy.sql.sqltypes import TIMESTAMP
+from sqlalchemy.sql.sqltypes import TIMESTAMP 
 from .database import Base
 
 # This is the structure of Post table in the database 
@@ -16,6 +14,7 @@ class Post(Base):
     image = Column(String)
     published = Column (Boolean, server_default='TRUE', nullable=False)
     created_at = Column(TIMESTAMP(timezone=True), nullable=False, server_default=text('now()'))
+    author = Column(Integer , ForeignKey("users.user_id",ondelete="CASCADE"), nullable=False)
 
 # This is the structure of user table in the database 
 class User(Base):
