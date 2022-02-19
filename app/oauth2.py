83beccfda5e6b8d7e1b3f64e213ad6f10cf93287
Abhishek kushwaha <abhishekkushwaha1479@gmail.com>
2022-02-19 18:42:23 +0530

    JWT FILE

---
+++ app/oauth2.py
@@ -0,0 +1,54 @@
+from jose import JWTError, jwt
+from datetime import datetime, timedelta
+from . import Schemas, database, models
+from fastapi import Depends, status, HTTPException
+from fastapi.security import OAuth2PasswordBearer
+from sqlalchemy.orm import Session
+# from .config import settings
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl='login')
+
+SECRET_KEY= "535835638646865444444444444454545446446456464646467464567869767695468656464646764674646546467"
+ALGORITHM="HS256"
+# ACCESS_TOKEN_EXPIRE_MINUTES=30
+
+# SECRET_KEY = settings.secret_key
+# ALGORITHM = settings.algorithm
+# ACCESS_TOKEN_EXPIRE_MINUTES = settings.access_token_expire_minutes
+
+
+def create_access_token(data: dict):
+    to_encode = data.copy()
+
+    # expire = datetime.utcnow() + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    # to_encode.update({"exp": expire})
+
+    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+
+    return encoded_jwt
+
+
+def verify_access_token(token: str, credentials_exception):
+
+    try:
+
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        username: str = payload.get("user_id")
+        if username is None:
+            raise credentials_exception
+        token_data = Schemas.TokenData(id=id)
+    except JWTError:
+        raise credentials_exception
+
+    return token_data
+
+
+def get_current_user(token: str = Depends(oauth2_scheme), db: Session = Depends(database.get_db)):
+    credentials_exception = HTTPException(status_code=status.HTTP_401_UNAUTHORIZED,
+                                          detail=f"Could not validate credentials", headers={"WWW-Authenticate": "Bearer"})
+
+    token = verify_access_token(token, credentials_exception)
+
+    user = db.query(models.User).filter(models.User.username == token.username).first()
+
+    return user
