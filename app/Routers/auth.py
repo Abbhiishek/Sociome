277e77bb40cf864ed212b71ea5218b6ab9a43255
Abhishek kushwaha <abhishekkushwaha1479@gmail.com>
2022-02-19 18:43:35 +0530

    login and authentication added

---
+++ app/Routers/auth.py
@@ -0,0 +1,30 @@
+from fastapi import APIRouter, Depends, status, HTTPException, Response
+from fastapi.security.oauth2 import OAuth2PasswordRequestForm
+from sqlalchemy.orm import Session
+
+from .. import database, Schemas, models ,oauth2
+from ..Routers import hashing
+
+router = APIRouter(tags=['Authentication'])
+
+
+@router.post('/login', response_model=Schemas.Token)
+def login(user_credentials: OAuth2PasswordRequestForm = Depends(), db: Session = Depends(database.get_db)):
+
+    user = db.query(models.User).filter(
+        models.User.username == user_credentials.username).first()
+
+    if not user:
+        raise HTTPException(
+            status_code=status.HTTP_403_FORBIDDEN, detail=f"Invalid Credentials")
+
+    if not hashing.verify_password(user_credentials.password, user.password):
+        raise HTTPException(
+            status_code=status.HTTP_403_FORBIDDEN, detail=f"Invalid Credentials")
+
+    # create a token
+    # return token
+
+    access_token = oauth2.create_access_token(data={"user_id": user.username})
+
+    return {"access_token": access_token, "token_type": "bearer"}
