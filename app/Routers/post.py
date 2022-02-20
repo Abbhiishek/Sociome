44b55cda1a1ebb9ff7e7a7f983d61713a367db63
Abhishek kushwaha <abhishekkushwaha1479@gmail.com>
2022-02-20 14:30:02 +0530

    got votes attached to get post features

--- app/Routers/post.py
+++ app/Routers/post.py
@@ -2,8 +2,8 @@ from csv import list_dialects
 from http.client import HTTPException
 # import psycopg2
 import time
-from typing import List
-from sqlalchemy import desc
+from typing import List, Optional
+from sqlalchemy import desc, func
 from sqlalchemy.orm import Session
 from psycopg2.extras import RealDictCursor
 from fastapi import  Response, status, HTTPException, Depends, APIRouter
@@ -18,19 +18,28 @@ router= APIRouter(prefix="/posts",
 
 
 # This path OPERATION is to retrieve all the post from the data base @
-@router.get("/",status_code=status.HTTP_200_OK , response_model=List[Schemas.Post]) #it get all the posts
-def get_posts(db: Session = Depends(get_db), current_user: int = Depends(oauth2.get_current_user)):
+@router.get("/",status_code=status.HTTP_200_OK , response_model=List[Schemas.PostOut]) #it get all the posts
+def get_posts(db: Session = Depends(get_db), current_user: int = Depends(oauth2.get_current_user) , limit: int = 10, skip: int = 0, search: Optional[str] = ""):
     # cur.execute("""SELECT * FROM posts  ORDER BY created_at DESC""")
     # posts=cur.fetchall()
-    posts=db.query(models.Post).order_by(Post.created_at.desc()).all()
+
+
+
+    posts = db.query(models.Post, func.count(models.Vote.post_id).label("votes")).join(
+        models.Vote, models.Vote.post_id == models.Post.post_id, isouter=True).order_by(Post.created_at.desc()).group_by(models.Post.post_id).filter(models.Post.content.contains(search)).limit(limit).offset(skip).all()
     return posts
 
 # This path OPERATION is to retrieve all the post of the logged in user !
-@router.get("/logged_user",status_code=status.HTTP_200_OK , response_model=List[Schemas.Post]) #it get all the posts
+@router.get("/logged_user",status_code=status.HTTP_200_OK , response_model=List[Schemas.PostOut]) #it get all the posts
 def get_yours_posts(db: Session = Depends(get_db), current_user: int = Depends(oauth2.get_current_user)):
     # cur.execute("""SELECT * FROM posts  ORDER BY created_at DESC""")
     # posts=cur.fetchall()
-    posts=db.query(models.Post).filter(models.Post.author == current_user.user_id).order_by(Post.created_at.desc()).all()
+    posts=db.query(models.Post , func.count(models.Vote.post_id).label("votes")).join(
+        models.Vote, models.Vote.post_id == models.Post.post_id, isouter=True).group_by(models.Post.post_id).filter(models.Post.author == current_user.user_id).order_by(Post.created_at.desc()).all()
+
+
+    # posts = db.query(models.Post, func.count(models.Vote.post_id).label("votes")).join(
+    #     models.Vote, models.Vote.post_id == models.Post.post_id, isouter=True).group_by(models.Post.post_id).filter(models.Post.post_id == id).first()
 
     if not posts:
         raise HTTPException(status_code = status.HTTP_404_NOT_FOUND,
@@ -56,11 +65,13 @@ def create_posts(content:Schemas.PostCreate, db: Session = Depends(get_db), curr
 
 
 #This path operation is to find a single post already in the database by the post id :
-@router.get("/{id}", status_code=status.HTTP_302_FOUND , response_model=Schemas.Post)
+@router.get("/{id}", status_code=status.HTTP_302_FOUND , response_model=Schemas.PostOut)
 def get_post(id : int, db: Session = Depends(get_db),current_user: int = Depends(oauth2.get_current_user)):
     # cur.execute(""" SELECT * FROM posts WHERE post_id = %s """, (str(id),))
     # post=cur.fetchone()
-    post=db.query(models.Post).filter(models.Post.post_id==id).first()
+    # post=db.query(models.Post).filter(models.Post.post_id==id).first()
+    post = db.query(models.Post, func.count(models.Vote.post_id).label("votes")).join(
+        models.Vote, models.Vote.post_id == models.Post.post_id, isouter=True).group_by(models.Post.post_id).filter(models.Post.post_id == id).first()
 
     if not post:
         raise HTTPException(status_code = status.HTTP_404_NOT_FOUND,
