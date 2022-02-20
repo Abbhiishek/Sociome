f1b7e5cce077f2590e31ecbd4bd707e6d40c95b5
Abhishek kushwaha <abhishekkushwaha1479@gmail.com>
2022-02-20 10:18:58 +0530

    configured the vote database  named votes as table name

--- app/models.py
+++ app/models.py
@@ -32,3 +32,11 @@ class User(Base):
     bio = Column(String)
     github_link = Column(String)
     joined_at = Column(TIMESTAMP(timezone=True), nullable=False, server_default=text('now()'))
+
+
+
+class Vote(Base):
+
+    __tablename__ = "votes"
+    user_id = Column (Integer, ForeignKey("users.user_id", ondelete="CASCADE") , primary_key=True, nullable=False )
+    post_id = Column (Integer, ForeignKey("posts.post_id", ondelete="CASCADE"), primary_key=True, nullable=False)
